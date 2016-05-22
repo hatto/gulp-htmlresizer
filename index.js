ada6856b01267d4c62e866e749a38680f7a76026
peter kracik <peter@kracik.sk>
2016-05-22 12:37:43 +0200

    clean js

--- index.js
+++ index.js
@@ -15,77 +15,106 @@ const contentTypes = {
   ".webp": "image/webp"
 }
 
-module.exports = function(file, base) {
-  base = base || process.cwd();
-  var htmlpath = path.join(base, path.dirname(file));
-  var html = fs.readFileSync(process.argv[2])
+module.exports = function(htmlFile) {
+  var image = null;
+  const base = process.cwd(),
+        htmlpath = path.join(base, path.dirname(htmlFile)),
+        html = fs.readFileSync(process.argv[2]),
+        dom = cheerio.load(String(html))
+  ;
 
-  var dom = cheerio.load(String(html));
-  htmlImageResize(dom);
-
+  var newhtml = htmlImageResize(dom);
+  console.log(newhtml);
   return new Buffer(dom.html({decodeEntities: false}));
 
   function htmlImageResize(dom) {
-    var styles = [];
     dom('img').each(function(idx, el) {
       el = dom(el);
-      var src = el.attr('src');
-      if (src && isLocal(src)) {
-        var dir = htmlpath + "/" + path.dirname(src);
-        var file = path.join(htmlpath, src);
-        var img = fs.readFileSync(file);
-        var contentType = contentTypes[path.extname(file)] || 'image/png';
+      const src = el.attr('src') || null,
+            width = el.attr('img-width') || null,
+            height = el.attr('img-height') || null
+      ;
+
+      // if no src or no attribute to resize or crop
+      if (!src || (!width && !height)) {
+        return true;
+      }
+
+      // image store in local
+      if (isLocal(src)) {
+        const dir = htmlpath + "/" + path.dirname(src),
+              file = path.join(htmlpath, src),
+              contentType = contentTypes[path.extname(file)] || 'image/png'
+        ;
         image = {
-            dir: dir,
-            file: file,
-            fileName: path.basename(file),
-            contentType: contentType,
-            resize: {
-                width: el.attr('img-width'),
-                height: el.attr('img-height'),
-                crop: el.attr('img-crop') === 'true'
-            }
+          dir: dir,
+          file: file,
+          fileName: path.basename(file),
+          contentType: contentType,
+          resize: {
+            width: width,
+            height: height,
+            crop: el.attr('img-crop') === 'true'
+          }
         };
-        var croppedImg = (image.resize.crop) ? crop(image) : resize(image);
-        return croppedImg;
-      } else if (src && !isLocal(src)) {
-        var file = src;
-        request.get(file, function (error, response, body) {
+        const croppedImg = (image.resize.crop) ? crop(image) : resize(image);
+        el.attr('src', croppedImg);
+        el.attr('img-width', null);
+        el.attr('img-height', null);
+        el.attr('img-crop', null);
+      } else { // external image
+        const extfile = src;
+        request.get(extfile, function (error, response, body) {
           if (!error && response.statusCode == 200) {
-            var img = body;
-            var contentType = contentTypes[path.extname(file)] || 'image/png';
+            const contentType = contentTypes[path.extname(file)] || 'image/png',
+                  file = src
+            ;
             image = {
               dir: htmlpath,
               file: file,
               fileName: path.basename(file),
               contentType: contentType,
               resize: {
-                width: el.attr('img-width'),
-                height: el.attr('img-height'),
+                width: width,
+                height: height,
                 crop: el.attr('img-crop') === 'true'
               }
             };
             var croppedImg = (image.resize.crop && (image.resize.width && image.resize.height)) ? crop(image) : resize(image);
-            return croppedImg;
+            el.attr('src', croppedImg);
+            el.attr('img-width', null);
+            el.attr('img-height', null);
+            el.attr('img-crop', null);
           }
+          return null;
         });
       }
-
     });
   }
 
+  /**
+   * determine if image is local or external
+   * @param  {String}  href   src attriute of the image
+   * @return {Boolean}        if it is local
+   */
   function isLocal(href) {
     return href && !url.parse(href).hostname;
   }
 
+  /**
+   * resize of the image
+   * @param  {Object} image   json object
+   * @return {String}         name of the image
+   */
   function resize(image) {
     const newname = createName(image);
+    // if image already exists
     fs.stat(newname, function(err, stat) {
       if(err == null) {
-       console.log('exists');
-       return false;
-     }
+        return false;
+      }
     });
+    // options for the resize
     const options = {
       srcPath: image.file,
       dstPath: newname,
@@ -97,12 +126,18 @@ module.exports = function(file, base) {
     if (image.resize.height) {
       options.height = image.resize.height;
     }
+    // resize image
     im.resize(options, function(err) {
       console.log(err);
     });
     return newname;
   }
 
+  /**
+   * crop image for the dimensions
+   * @param  {Object} image   json object
+   * @return {String}         new image name
+   */
   function crop(image) {
     const newname = createName(image);
     fs.stat(newname, function(err, stat) {
@@ -123,6 +158,11 @@ module.exports = function(file, base) {
     return newname;
   }
 
+  /**
+   * get image name after resize
+   * @param  {Object} image   json object
+   * @return {String}         new image name
+   */
   function createName(image) {
     const extension = path.extname(image.file);
     var newFileName = "";
@@ -134,7 +174,6 @@ module.exports = function(file, base) {
     }
     newFileName += extension;
     const newname = path.join(image.dir, image.fileName.replace(extension, newFileName));
-    console.log(newname);
     return newname;
   }
 }
